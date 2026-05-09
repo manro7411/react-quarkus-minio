1ad9272c37cb20b285cb09eacea8484a69efc519
Ratchanon Traitiprat <teenoigammer2546@gmail.com>
2026-05-09 19:56:39 +0700

    [UPDATE] -> admin-vite.conf

--- admin-portal/vite.config.ts
+++ admin-portal/vite.config.ts
@@ -3,12 +3,24 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+
   server: {
     host: "0.0.0.0",
     port: 5175,
+    allowedHosts: [
+      "admin.n9ne.cc",
+      "119.59.103.227",
+      "localhost",
+    ],
   },
+
   preview: {
     host: "0.0.0.0",
     port: 5175,
+    allowedHosts: [
+      "admin.n9ne.cc",
+      "119.59.103.227",
+      "localhost",
+    ],
   },
 });
